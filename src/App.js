6eb09150a8b607272d84e5c3f5903c04f2a51bee
Le Deng <ray.deng83@gmail.com>
2019-09-15 18:07:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Test
         </a>
       </header>
     </div>
